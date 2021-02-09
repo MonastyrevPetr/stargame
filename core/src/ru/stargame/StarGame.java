68a5f7fcf10243b5d135ba1dbad134681b85a307
Petr <monastyrevpetr@gmail.com>
2021-02-09 17:42:33 +0300

    hw

--- core/src/ru/stargame/StarGame.java
+++ core/src/ru/stargame/StarGame.java
@@ -1,17 +1,15 @@
 package ru.stargame;
 
 import com.badlogic.gdx.Game;
+
+
 import ru.stargame.screen.MenuScreen;
 
 public class StarGame extends Game {
 
-
-	
 	@Override
 	public void create () {
 		setScreen(new MenuScreen());
-
 	}
-
 
 }
--- core/src/ru/stargame/screen/MenuScreen.java
+++ core/src/ru/stargame/screen/MenuScreen.java
@@ -1,79 +1,49 @@
 package ru.stargame.screen;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
+
 import ru.stargame.base.BaseScreen;
+import ru.stargame.math.Rect;
+import ru.stargame.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
-    private Texture img;
-    private Texture background;
-    private Vector2 touch;
-    private  Vector2 v;
-    private Vector2 n;
+
+    private Texture bg;
+    private Background background;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
-        background = new Texture("background.jpg");
-        touch = new Vector2();
-        v=new Vector2();
-        n=new Vector2();
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0.5f, 0.5f, 1, 0.5f);
+        Gdx.gl.glClearColor(0.3f, 0.6f, 0.4f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(img, touch.x, touch.y);
+        background.draw(batch);
         batch.end();
-        touch.add(v);
-        if (Math.abs(touch.x-n.x)<=1 && Math.abs(touch.y-n.y)<=1){
-            v.set(0,0);
-        }
     }
 
     @Override
     public void dispose() {
-        img.dispose();
-        background.dispose();
+        bg.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        v.set(screenX, Gdx.graphics.getHeight() - screenY).sub(touch);
-        v.nor();
-        n.set(screenX,Gdx.graphics.getHeight() - screenY);
-        return false;
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
     }
 
     @Override
-    public boolean keyDown(int keycode) {
-        switch (keycode) {
-            case Input.Keys.UP:
-                touch.y += 10;
-                v.set(0,0);
-                break;
-            case Input.Keys.DOWN:
-                touch.y -= 10;
-                v.set(0,0);
-                break;
-            case Input.Keys.LEFT:
-                touch.x -= 10;
-                v.set(0,0);
-                break;
-            case Input.Keys.RIGHT:
-                touch.x += 10;
-                v.set(0,0);
-                break;
-        }
-        return false;
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return super.touchDown(touch, pointer, button);
     }
 }
